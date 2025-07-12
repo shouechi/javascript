7935caa7c8e886d1ec522abfbdd8a6848edebfbd
shouechi <hajimete.1105@gmail.com>
2025-07-12 23:56:26 +0900

    親子間のノード取得

--- section15.js
+++ section15.js
@@ -1,4 +1,10 @@
 let result = document.documentElement; //HTML全体を取得する。
 result = document.head; //head要素を取得する。
 result = document.body; //body要素を取得する。
+result = document.body.childNodes //body要素の子要素を取得する。
+result = document.body.firstChild; //body要素の最初の子要素を取得する。
+result = document.body.lastChild; //body要素の最後の子要素を取得する。
+result = document.body.parentNode; //body要素の親要素を取得する。
+result = document.body.nextSibling; //body要素の次の兄弟要素を取得する。
+result = document.body.previousSibling; //body要素の前の兄弟要素を取得する。
 console.log(result);
