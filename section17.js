710afe795e1825f2564a1a3e18428df0b4c06de4
shouechi <hajimete.1105@gmail.com>
2025-08-12 23:58:08 +0900

    Promiseのresolveの動き、Promiseチェーンについて

--- section17.js
+++ section17.js
@@ -33,13 +33,21 @@ let promise =new Promise((resolve, reject) =>{
   resolve('error');//関数オブジェクトになる。変更は１度だけになる。
   // console.log('new promise');
 }); //第１引数にコールバック関数を入れいないといけない。
-promise = new Promise(() => {})//実行さしてもないも起きない。
-promise =new Promise((resolve, reject) =>{
-  // resolve(); //この場合は、thenとfinallyが実行される。
-  setTimeout(() => {
-    reject(new Error('error')); //この場合は、catchとfinallyが実行される。
-  },1000); 
-}); //resolve, rejectを使い回すには、非同期処理を使用する。
+promise = new Promise((resolve) => {
+  let tmpPromise = new Promise((resolve2) => {
+    setTimeout(() => {
+      resolve2()
+    }, 1000);
+  });
+  resolve(tmpPromise);//先に内側のpromiseが実行されてから、最後に外側が実行される。
+})//実行さしてもないも起きない。
+promise = new Promise(() => {});
+// promise =new Promise((reject) =>{
+//   // resolve(); //この場合は、thenとfinallyが実行される。
+//   setTimeout(() => {
+//     reject(new Error('error')); //この場合は、catchとfinallyが実行される。
+//   },1000); 
+// }); //resolve, rejectを使い回すには、非同期処理を使用する。
 promise.then((value) => {
   console.log('then', value);
 }, (error) => {
@@ -51,4 +59,54 @@ promise.catch(() => {
 promise.finally(() => {
   console.log('finally')
 })//何も値を返さない。
+new Promise((resolve) => resolve(1))
+  .then((value) => {
+    console.log(value);
+    return 2;//returnすること次にthenに移り、第１引数に入る。
+  })
+  .then((value) => {
+    console.log(value);
+    throw new Error(3); //throwの場合は次のcatchに移り、第１引数に入る。
+  })
+  .catch((error) => {
+    console.log(error.message);
+    throw new Error(4); //throwの場合は次のcatchに移り、第１引数に入る。
+  }) //いくでもthen、catchを繋げることができる。
+  .catch((error) => {
+    console.log(error.message);
+    return 5; //returnすること次にthenに移り、第１引数に入る。
+  })
+  .then((value) => {
+    console.log(value);
+    return 6; //次がcatchだった場合は、処理がスキップされる。
+  })
+  .catch(() => console.log('skip'))
+  .then((value) => {
+    console.log(value);//ruturnでの６が第一引数に入る。
+    throw new Error(7);
+  })//returnの場合はthenを探して、引数を渡す。
+  .then(() => console.log('skip'))
+  .catch((error) => {
+    console.log(error.message);
+    return 8;
+  })//throwの場合はcatchを探して、引数を渡す。最後まで探してcatchがない場合はエラーが出る。
+  .catch(() => console.log('skip'))
+  .finally((value) => {
+    console.log('finally value: ', value)//第１引数には何も入らない。thenを探す。
+    return new Promise((resolve) => {//この場合は、1秒待ってから8が表示される。
+      setTimeout(() => {
+        resolve();
+      }, 1000)
+    })
+  })
+  .then((value) => {
+    console(value);
+  })
+  .finally(() => {
+    throw new Error(9); //この場合はcatch探しが始まる。
+  })
+  .catch((error) => {
+    console.log(error.messsage);
+  })
 console.log(promise);
+
